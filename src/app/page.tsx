74407d33cde5b4b29836fa28cfbd11316764e362
nrsimonelli <nrsimonelli@gmail.com>
2024-07-19 22:25:36 -0600

    misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import yeti from '@/assets/yeti.png'
 
 export default function Home() {
   return (
-    <main className='p-24 flex flex-col justify-center items-center space-y-12'>
+    <main className='p-4 min-h-screen sm:p-24 flex flex-col justify-center items-center space-y-12'>
       <div>
         <p className={'text-4xl sm:text-6xl font-bold'}>Coming soon!</p>
       </div>
